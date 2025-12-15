90bd98e09f8a31d0d860f3780814431346ed8601
ilanalewitton2 <145459814+ilanalewitton2@users.noreply.github.com>
2025-12-15 11:35:14 -0500

    12/15 main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/discovery_page.dart';
 
 void main() {
+  Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://YOUR_PROJECT_ID.supabase.co',
+    anonKey: 'YOUR_ANON_PUBLIC_KEY',
+  );
+
   runApp(const PSINTApp());
+}
 }
 
 class PSINTApp extends StatelessWidget {
